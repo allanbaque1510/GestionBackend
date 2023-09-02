4e3c26a59f95bc006b33e8bcbdeb74cb38c4ff47
allanbaque1510 <56411730+allanbaque1510@users.noreply.github.com>
2023-09-02 09:46:57 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import app from "./app.js"
 import { connectDB } from "./db.js";
 connectDB();
-app.listen(3000);
-console.log("Server on port", 3000)
+const PORT = process.env.PORT || 3000
+app.listen(PORT);
+console.log("Server on port", PORT)
